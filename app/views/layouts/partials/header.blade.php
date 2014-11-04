86ff69c1a03bfec9d9a3e9b718bc800a2e3e5e40
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-04 12:00:56 +0100

    Use tabs.

--- app/views/layouts/partials/header.blade.php
+++ app/views/layouts/partials/header.blade.php
@@ -1,18 +1,18 @@
 <nav class="navbar navbar-default">
-    <div class="container">
-        {{ link_to(URL::to('/'), 'Phragile', ['class' => 'navbar-brand']) }}
+	<div class="container">
+		{{ link_to(URL::to('/'), 'Phragile', ['class' => 'navbar-brand']) }}
 
-        <ul class="nav navbar-nav navbar-right">
-            <li>
-                {{ link_to(
-                    $_ENV['PHABRICATOR_URL'] . 'oauthserver/auth/?' . http_build_query([
-                         'response_type' => 'code',
-                         'client_id' => $_ENV['OAUTH_CLIENT_ID'],
-                         'redirect_uri' => URL::to('/'),
-                    ]),
-                    'Log in using Phabricator'
-                ) }}
-            </li>
-        </ul>
-    </div>
+		<ul class="nav navbar-nav navbar-right">
+			<li>
+				{{ link_to(
+					$_ENV['PHABRICATOR_URL'] . 'oauthserver/auth/?' . http_build_query([
+						 'response_type' => 'code',
+						 'client_id' => $_ENV['OAUTH_CLIENT_ID'],
+						 'redirect_uri' => route('login_path'),
+					]),
+					'Log in using Phabricator'
+				) }}
+			</li>
+		</ul>
+	</div>
 </nav>
