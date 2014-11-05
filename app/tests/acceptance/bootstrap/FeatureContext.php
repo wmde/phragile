3c4cafe51e9a2ae1cf13213bd1d04880795f9e10
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-05 15:24:50 +0100

    Add OAuth login tests.

--- app/tests/acceptance/bootstrap/FeatureContext.php
+++ app/tests/acceptance/bootstrap/FeatureContext.php
@@ -10,30 +10,69 @@ use Behat\MinkExtension\Context\MinkContext;
  */
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
+	protected $params;
+
+	/**
+	 * Initializes context.
+	 *
+	 * Every scenario gets its own context instance.
+	 * You can also pass arbitrary arguments to the
+	 * context constructor through behat.yml.
+	 */
+	public function __construct($params)
+	{
+		$this->params = $params;
+	}
+
+	/**
+	 * Setup Laravel
+	 *
+	 * @beforeSuite
+	 */
+	public static function bootstrapLaravel()
+	{
+		$app = require_once __DIR__ . '/../../../../bootstrap/start.php';
+		$app->boot();
+	}
+
+	/**
+	 * @Given I am not logged in
+	 */
+	public function iAmNotLoggedIn()
+	{
+		$this->visit('/logout');
+	}
+
+	/**
+	 * @When Phabricator authorizes me
+	 */
+	public function phabricatorAuthorizesMe()
+	{
+		$this->phabricatorLogin();
+	}
+
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Then I should not be logged in
      */
-    public function __construct()
+    public function iShouldNotBeLoggedIn()
     {
+		$this->assertPageContainsText('Log in using Phabricator');
     }
 
     /**
-     * @Given I am not logged in
+     * @Given I am logged in
      */
-    public function iAmNotLoggedIn()
+    public function iAmLoggedIn()
     {
-        throw new PendingException();
+        $this->clickLink('Log in');
+		$this->phabricatorLogin();
     }
 
-    /**
-     * @When Phabricator authorizes me
-     */
-    public function phabricatorAuthorizesMe()
-    {
-        throw new PendingException();
-    }
+	private function phabricatorLogin()
+	{
+		$this->fillField('username', $this->params['phabricator_username']);
+		$this->fillField('password', $this->params['phabricator_password']);
+		$this->pressButton('Login');
+		$this->clickLink('Continue');
+	}
 }
--- app/tests/acceptance/oauth_login.feature
+++ app/tests/acceptance/oauth_login.feature
@@ -5,6 +5,18 @@ Feature: OAuth Login
 
   Scenario: Log in
     Given I am not logged in
-    When I follow "Sign in using Phabricator"
+    When I follow "Log in using Phabricator"
     And Phabricator authorizes me
-    Then I should see "Logged in as"
+    Then I should see "you are now logged in"
+
+  Scenario: Log in with invalid access token
+    Given I am not logged in
+    When I go to "/login?code=abc123"
+    Then I should see "Login failed"
+    And I should not be logged in
+
+  Scenario: Log out
+    Given I am logged in
+    When I follow "Logout"
+    Then I should see "You are now logged out."
+    And I should not be logged in
