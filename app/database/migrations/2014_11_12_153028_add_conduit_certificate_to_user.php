6776f51486e1a7b83c877300a20732c4ad2e9a06
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-12 18:08:47 +0100

    Add migration for user's Conduit certificates.

---
+++ app/database/migrations/2014_11_12_153028_add_conduit_certificate_to_user.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConduitCertificateToUser extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->text('conduit_certificate');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropColumn('conduit_certificate');
+		});
+	}
+
+}
