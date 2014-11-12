efeb407389f1a3c16d5f123c8097277b04571900
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-12 16:22:33 +0100

    Show modal for the conduit certificate when clicking the respective menu button.

--- app/views/layouts/partials/header.blade.php
+++ app/views/layouts/partials/header.blade.php
@@ -10,9 +10,45 @@
 						<span class="caret"></span>
 					</a>
 					<ul class="dropdown-menu" role="menu">
+						<li>
+							{{ link_to(
+								'#',
+								'Conduit certificate',
+								[
+									'id' => 'conduit-certificate',
+									'data-toggle' => 'modal',
+									'data-target' => '#conduit-modal',
+								]
+							) }}
+						</li>
 						<li>{{ link_to_route('logout_path', 'Logout') }}</li>
 					</ul>
 				</li>
+
+				<div class="modal fade" id="conduit-modal">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+								<h4 class="modal-title">Conduit certificate</h4>
+							</div>
+							<div class="modal-body">
+								<p>
+									Copy and paste your Conduit certificate from Phabricator here.
+									You can find it at
+									{{ link_to(
+										$_ENV['PHABRICATOR_URL'] . '/settings/panel/conduit/',
+										'Phabricator &gt; Settings &gt; Conduit Certificate'
+									) }}
+								</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								<button type="button" class="btn btn-primary">Save changes</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			@else
 				{{ link_to(
 					$_ENV['PHABRICATOR_URL'] . 'oauthserver/auth/?' . http_build_query([
