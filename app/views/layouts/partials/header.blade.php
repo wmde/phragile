4465ed440751a2175c28f80edc7433bbce054201
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-13 12:04:51 +0100

    Add form for certificate updates.

--- app/views/layouts/partials/header.blade.php
+++ app/views/layouts/partials/header.blade.php
@@ -32,20 +32,33 @@
 								<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
 								<h4 class="modal-title">Conduit certificate</h4>
 							</div>
-							<div class="modal-body">
-								<p>
-									Copy and paste your Conduit certificate from Phabricator here.
-									You can find it at
-									{{ link_to(
-										$_ENV['PHABRICATOR_URL'] . '/settings/panel/conduit/',
-										'Phabricator &gt; Settings &gt; Conduit Certificate'
-									) }}
-								</p>
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-								<button type="button" class="btn btn-primary">Save changes</button>
-							</div>
+							{{ Form::model(Auth::user(), ['route' => 'conduit_certificate_path', 'method' => 'PUT']) }}
+								<div class="modal-body">
+									<p>
+										Copy and paste your Conduit certificate from Phabricator here.
+										You can find it at
+										{{ link_to(
+											$_ENV['PHABRICATOR_URL'] . '/settings/panel/conduit/',
+											'Phabricator &gt; Settings &gt; Conduit Certificate'
+										) }}
+									</p>
+									<p>
+										{{ Form::textarea(
+											'conduit_certificate',
+											null,
+											[
+												'class' => 'form-control',
+												'rows' => 3,
+												'placeholder' => 'Conduit Certificate',
+											]
+										) }}
+									</p>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+									<button type="submit" class="btn btn-primary">Submit</button>
+								</div>
+							{{ Form::close() }}
 						</div>
 					</div>
 				</div>
