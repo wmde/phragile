90d08172cd9ba6d1695524598a08a411d8b31762
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-26 15:34:30 +0100

    Create partial for conduit certificate form.

--- app/views/layouts/partials/header.blade.php
+++ app/views/layouts/partials/header.blade.php
@@ -24,44 +24,8 @@
 						<li>{{ link_to_route('logout_path', 'Logout') }}</li>
 					</ul>
 				</li>
-
-				<div class="modal fade" id="conduit-modal">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-								<h4 class="modal-title">Conduit certificate</h4>
-							</div>
-							{{ Form::model(Auth::user(), ['route' => 'conduit_certificate_path', 'method' => 'PUT']) }}
-								<div class="modal-body">
-									<p>
-										Copy and paste your Conduit certificate from Phabricator here.
-										You can find it at
-										{{ link_to(
-											$_ENV['PHABRICATOR_URL'] . '/settings/panel/conduit/',
-											'Phabricator &gt; Settings &gt; Conduit Certificate'
-										) }}
-									</p>
-									<p>
-										{{ Form::textarea(
-											'conduit_certificate',
-											null,
-											[
-												'class' => 'form-control',
-												'rows' => 4,
-												'placeholder' => 'Conduit Certificate',
-											]
-										) }}
-									</p>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-									<button type="submit" class="btn btn-primary">Submit</button>
-								</div>
-							{{ Form::close() }}
-						</div>
-					</div>
-				</div>
+				
+				@include('layouts.partials.conduit_certificate_form')
 			@else
 				{{ link_to(
 					$_ENV['PHABRICATOR_URL'] . 'oauthserver/auth/?' . http_build_query([
