ad2115f9794a4897d98898b39556eea7bf92d6c9
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-26 17:12:41 +0100

    Add form for sprint creation.

--- app/views/sprint/create.blade.php
+++ app/views/sprint/create.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
 	<h1>Create sprint for {{ $project->title }}</h1>
+
+	{{ Form::open(['method' => 'POST', 'route' => ['store_sprint_path', $project->id]]) }}
+		<div class="form-group">
+			{{ Form::label('title', 'Title:') }}
+			{{ Form::text('title', '', ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('sprint_start', 'Sprint start:') }}
+			{{ Form::text('sprint_start', '', ['class' => 'form-control']) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('sprint_end', 'Sprint end:') }}
+			{{ Form::text('sprint_end', '', ['class' => 'form-control']) }}
+		</div>
+
+		{{ Form::submit('Create new sprint', ['class' => 'btn btn-primary']) }}
+	{{ Form::close() }}
 @stop
