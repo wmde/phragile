66209ccac60dc28efb2505691a2f6da249e34825
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-11-27 16:24:22 +0100

    Add IDs to sprints.

---
+++ app/database/migrations/2014_11_27_152120_add_sprints_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSprintsId extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('sprints', function($t)
+		{
+			$t->increments('id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('sprints', function($t)
+		{
+			$t->dropColumn('id');
+		});
+	}
+
+}
