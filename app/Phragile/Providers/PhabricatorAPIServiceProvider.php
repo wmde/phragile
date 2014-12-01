3ce85074636877ac855aaa40dedd702a4118e379
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-12-01 13:49:35 +0100

    Do Phabricator API queries through the Phragile bot by default.

--- app/Phragile/Providers/PhabricatorAPIServiceProvider.php
+++ app/Phragile/Providers/PhabricatorAPIServiceProvider.php
@@ -8,7 +8,10 @@ class PhabricatorAPIServiceProvider extends ServiceProvider {
 	public function register()
 	{
 		$this->app->singleton('phabricator', function() {
-			return new PhabricatorAPI(new \ConduitClient($_ENV['PHABRICATOR_URL']));
+			$phabricator = new PhabricatorAPI(new \ConduitClient($_ENV['PHABRICATOR_URL']));
+			$phabricator->connect($_ENV['PHRAGILE_BOT_NAME'], $_ENV['PHRAGILE_BOT_CERTIFICATE']);
+
+			return $phabricator;
 		});
 	}
 }
