3933843da3390e7a5403b77cd8059457b4ca04bf
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-12-02 14:18:24 +0100

    Add dropdown for sprint list on sprint overview page. Closes #11

--- app/controllers/ProjectsController.php
+++ app/controllers/ProjectsController.php
@@ -4,6 +4,8 @@ class ProjectsController extends BaseController {
 
 	public function show(Project $project)
 	{
-		return View::make('project.view', compact('project'));
+		$currentSprint = Sprint::where('sprint_start', '<=', 'CURRENT_DATE')->take(1)->first();
+
+		return View::make('project.view', compact('project', 'currentSprint'));
 	}
 }
