af63fd1547d40183aefeefb1d4f4329d3a6acbe4
Jakob Warkotsch <j.warkotsch@gmail.com>
2014-12-03 17:52:19 +0100

    Prettify dropdown.

--- app/views/sprint/view.blade.php
+++ app/views/sprint/view.blade.php
@@ -2,16 +2,26 @@
 
 @section('content')
 	<h1 class="sprint-overview-title">
-		{{ $project->title }} Sprint Overview:
-		<select id="sprint-select" class="sprint-select">
-			@foreach($project->sprints as $sprint)
-				@if($currentSprint && $currentSprint->id === $sprint->id)
-					<option value="{{ $sprint->id }}" selected>Current Sprint ({{ $sprint->title }})</option>
-				@else
-					<option value="{{ $sprint->id }}">{{ $sprint->title }}</option>
-				@endif
-			@endforeach
-		</select>
+		{{ $sprint->project->title }} Sprint Overview:
+		<span class="dropdown">
+			<button class="btn btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+				{{ $sprint->title }}
+                <span class="caret"></span>
+			</button>
+			<ul class="dropdown-menu">
+				@foreach($sprint->project->sprints as $s)
+					<li class="{{ $s->id === $sprint->id ? 'active' : '' }}">
+						<a href="{{ route('sprint_path', $s->phid) }}">
+							@if($currentSprint && $currentSprint->id === $s->id)
+								Current sprint ({{ $s->title }})
+							@else
+								{{ $s->title }}
+							@endif
+						</a>
+					</li>
+				@endforeach
+			</ul>
+		</span>
 	</h1>
 
 	{{ HTML::ul($taskList->getTasks()) }}
