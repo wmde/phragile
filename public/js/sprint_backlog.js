242bcaf2f3d8d7635f9493400531f80bb40aba09
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-01-12 14:16:36 +0100

    Refactor sprint_backlog.js.

--- public/js/sprint_backlog.js
+++ public/js/sprint_backlog.js
@@ -1,16 +1,16 @@
-var options = {
-    valueNames: ['title', 'points', 'status', 'priority']
-};
+(function () {
+    var backlog = new List('backlog', {
+            valueNames: ['title', 'points', 'status', 'priority']
+        }),
+        $resetBtn = $('#reset-filter');
 
-var backlog = new List('backlog', options),
-    $resetBtn = $('#reset-filter');
+    $('.filter-backlog').on('click', function () {
+        backlog.search($(this).data('value'), [$(this).data('column')]);
+        $resetBtn.prop('disabled', backlog.items.length === backlog.visibleItems.length);
+    });
 
-$('.filter-backlog').on('click', function () {
-    backlog.search($(this).data('value'), [$(this).data('column')]);
-    $resetBtn.prop('disabled', backlog.items.length === backlog.visibleItems.length);
-});
-
-$resetBtn.on('click', function () {
-    backlog.search();
-    $resetBtn.prop('disabled', true);
-});
+    $resetBtn.on('click', function () {
+        backlog.search();
+        $resetBtn.prop('disabled', true);
+    });
+})();
