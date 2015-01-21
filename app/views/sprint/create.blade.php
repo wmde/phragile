3d0f6080a9878256b56129adebc516faa2def9ea
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-01-21 17:14:13 +0100

    Suggest sprint title based on start date.

--- app/views/sprint/create.blade.php
+++ app/views/sprint/create.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>Create sprint for {{ $project->title }}</h1>
+	<h1>Create sprint for <span id="project-title">{{ $project->title }}</span></h1>
 
 	{{ Form::open(['method' => 'POST', 'route' => ['store_sprint_path', $project->slug]]) }}
 		<div class="form-group">
-			{{ Form::label('title', 'Title:') }}
-			{{ Form::text('title', '', ['class' => 'form-control']) }}
-		</div>
-
-		<div class="form-group">
 			{{ Form::label('sprint_start', 'Sprint start:') }}
-			{{ Form::text('sprint_start', '', ['class' => 'form-control datepicker']) }}
+			{{ Form::text('sprint_start', '', ['class' => 'form-control datepicker start']) }}
 		</div>
 
 		<div class="form-group">
 			{{ Form::label('sprint_end', 'Sprint end:') }}
-			{{ Form::text('sprint_end', '', ['class' => 'form-control datepicker']) }}
+			{{ Form::text('sprint_end', '', ['class' => 'form-control datepicker end']) }}
+		</div>
+
+		<div class="form-group">
+			{{ Form::label('title', 'Title:') }}
+			{{ Form::text('title', '', ['class' => 'form-control', 'id' => 'sprint-title']) }}
 		</div>
 
 		{{ Form::submit('Create new sprint', ['class' => 'btn btn-primary']) }}
@@ -27,10 +27,20 @@
 	{{ HTML::script('js/bootstrap-datepicker.js') }}
 
 	<script type="text/javascript">
-		$('.datepicker').datepicker({
+		var settings = {
 			format: 'yyyy-mm-dd',
 			autoclose: true
-		});
+		};
+
+		$('.datepicker.start').datepicker(settings)
+				.on('changeDate', function (e) {
+					$('#sprint-title').val(
+							'§ '
+							+ $('#project-title').text()
+							+ '-Sprint-'
+							+ e.format(settings.format));
+				});
+		$('.datepicker.end').datepicker(settings);
 	</script>
 @stop
 
