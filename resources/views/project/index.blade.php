b2561e4b6974fe4006306b1cb22f849a258edc4a
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-02-09 15:47:16 +0100

    Darken primary brand color.

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -48,7 +48,7 @@
 	<footer class="container">
 		<hr/>
 		<p>
-			Built by <a href="http://github.com/jakobw">Jakob Warkotsch</a> as a thesis project at <a href="http://fu-berlin.de">Freie Universität Berlin</a> in cooperation with <a href="http://wikimedia.de">Wikimedia Deutschland</a>
+			Built by <a href="http://github.com/jakobw">Jakob Warkotsch</a> as a thesis project at <a href="http://fu-berlin.de">Freie Universität Berlin</a> in cooperation with <a href="http://wikimedia.de">Wikimedia Deutschland</a>.
 		</p>
 	</footer>
 @stop
