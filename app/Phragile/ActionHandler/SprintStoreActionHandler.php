23f367901c3c88a957ae8b6be766a3b02da4c8ae
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-05-12 13:47:31 +0200

    Expand if-statement.

--- app/Phragile/ActionHandler/SprintStoreActionHandler.php
+++ app/Phragile/ActionHandler/SprintStoreActionHandler.php
@@ -79,8 +79,13 @@ class SprintStoreActionHandler {
 	private function connectWithPhabricatorProject()
 	{
 		$this->sprint->connectWithPhabricatorProject($this->fetchPhabricatorProject());
-		if ($this->sprint->save()) $this->redirectWithSuccessMessage('Connected "' . $this->sprint->title . '" with an existing Phabricator project');
-		else $this->redirectBackWithError('Could not create the sprint or connect it with an existing Phabricator project.');
+		if ($this->sprint->save())
+		{
+			$this->redirectWithSuccessMessage('Connected "' . $this->sprint->title . '" with an existing Phabricator project');
+		} else
+		{
+			$this->redirectBackWithError('Could not create the sprint or connect it with an existing Phabricator project.');
+		}
 	}
 
 	private function fetchPhabricatorProject()
