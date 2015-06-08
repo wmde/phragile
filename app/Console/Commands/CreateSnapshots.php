37ef4c00b1aa20049a6ca30b7ad54138635937fd
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-08 12:32:44 +0200

    Implement command for automatic snapshots.

---
+++ app/Console/Commands/CreateSnapshots.php
@@ -0,0 +1,34 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Sprint;
+
+class CreateSnapshots extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'snapshots:create';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'This command creates snapshots of all sprints in Phragile.';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		foreach (Sprint::all() as $sprint)
+		{
+			$sprint->createSnapshot();
+		}
+	}
+}
