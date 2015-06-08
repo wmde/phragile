71c13c8e7a0ab1a7c4281af384253b761d609ef6
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-08 12:35:16 +0200

    Remove less than useful comments.

--- app/Console/Commands/CreateSnapshots.php
+++ app/Console/Commands/CreateSnapshots.php
@@ -5,18 +5,7 @@ use Sprint;
 
 class CreateSnapshots extends Command {
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
 	protected $name = 'snapshots:create';
-
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
 	protected $description = 'This command creates snapshots of all sprints in Phragile.';
 
 	/**
