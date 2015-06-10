0e7df0a2537ba4f814c8710f6e8e00b32f256313
Bene <benestar.wikimedia@gmail.com>
2015-06-10 15:09:20 +0200

    Support multiple filters

--- resources/js/sprint_backlog.js
+++ resources/js/sprint_backlog.js
@@ -2,15 +2,37 @@
     var backlog = new List('backlog', {
             valueNames: ['title', 'points', 'status', 'priority', 'assignee']
         }),
-        $resetBtn = $('#reset-filter');
+        $resetBtn = $('#reset-filter'),
+		currentFilters = {};
 
     $('.filter-backlog').on('click', function () {
-        backlog.search($(this).data('value'), [$(this).data('column')]);
+		var value = $(this).data('value'),
+			column = $(this).data('column');
+
+		if (currentFilters[column] == value) {
+			delete currentFilters[column];
+		} else {
+			currentFilters[column] = value;
+		}
+
+		backlog.filter(function(item) {
+			var values = item.values();
+
+			for (var c in currentFilters) {
+				if (values[c].trim() != currentFilters[c]) {
+					return false;
+				}
+			}
+
+			return true;
+		});
+
         $resetBtn.prop('disabled', backlog.items.length === backlog.visibleItems.length);
     });
 
     $resetBtn.on('click', function () {
-        backlog.search();
+        backlog.filter();
+		currentFilters = {};
         $resetBtn.prop('disabled', true);
     });
 })();
