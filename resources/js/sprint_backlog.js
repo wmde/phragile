d5c273803c0b34bf186587c3074f373b90144ad1
Bene <benestar.wikimedia@gmail.com>
2015-06-19 12:39:41 +0200

    Fix spacing

--- resources/js/sprint_backlog.js
+++ resources/js/sprint_backlog.js
@@ -3,36 +3,36 @@
             valueNames: ['title', 'points', 'status', 'priority', 'assignee']
         }),
         $resetBtn = $('#reset-filter'),
-		currentFilters = {};
+        currentFilters = {};
 
     $('.filter-backlog').on('click', function () {
-		var value = $(this).data('value'),
-			column = $(this).data('column');
+        var value = $(this).data('value'),
+            column = $(this).data('column');
 
-		if (currentFilters[column] == value) {
-			delete currentFilters[column];
-		} else {
-			currentFilters[column] = value;
-		}
+        if (currentFilters[column] == value) {
+            delete currentFilters[column];
+        } else {
+            currentFilters[column] = value;
+        }
 
-		backlog.filter(function(item) {
-			var values = item.values();
+        backlog.filter(function(item) {
+            var values = item.values();
 
-			for (var c in currentFilters) {
-				if (values[c].trim() != currentFilters[c]) {
-					return false;
-				}
-			}
+            for (var c in currentFilters) {
+                if (values[c].trim() != currentFilters[c]) {
+                    return false;
+                }
+            }
 
-			return true;
-		});
+            return true;
+        });
 
         $resetBtn.prop('disabled', backlog.items.length === backlog.visibleItems.length);
     });
 
     $resetBtn.on('click', function () {
         backlog.filter();
-		currentFilters = {};
+        currentFilters = {};
         $resetBtn.prop('disabled', true);
     });
 })();
