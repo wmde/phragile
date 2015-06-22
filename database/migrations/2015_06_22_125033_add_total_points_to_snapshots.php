c75bab30f33ae75a96bf80aeee35d836a5a2ea8a
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-06-22 15:51:58 +0200

    Add migration for total points on snapshots.

---
+++ database/migrations/2015_06_22_125033_add_total_points_to_snapshots.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTotalPointsToSnapshots extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('sprint_snapshots', function($t)
+		{
+			$t->integer('total_points');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('sprint_snapshots', function($t)
+		{
+			$t->dropColumn('total_points');
+		});
+	}
+
+}
