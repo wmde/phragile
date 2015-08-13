7b10389832f127ca18e374845b0d3ecb843920c0
Bene <benestar.wikimedia@gmail.com>
2015-08-13 12:25:06 +0200

    Show daily closed points in burndown tooltip

--- resources/js/bar_chart.js
+++ resources/js/bar_chart.js
@@ -18,13 +18,27 @@ var PHRAGILE = PHRAGILE || {};
         constructor: PHRAGILE.BarChart,
 
         /**
+         * @param i - The data list index that the mouse is hovering
+         * @returns {string} - The label HTML
+         */
+        getLabelHTML: function (i) {
+            return '<tr class="' + this.cssID + '"'
+                + ' style="display: ' + ($('.bar.' + this.cssID).css('display') === 'none' ? 'none' : '') + '">'
+                + '<td>' + this.label + '</td>'
+                + '<td class="graph-value">'
+                + Math.round(this.data[Math.min(this.data.length - 1, i)].points)
+                + '</td>'
+                + '</tr>';
+        },
+
+        /**
          * Renders the bar charts inside the burndown/burnup chart
          */
         render: function () {
             d3.select('#graphs').selectAll(this.cssID)
                 .data(this.data)
                 .enter().append('line')
-                .attr('class', 'daily-points')
+                .attr('class', 'bar ' + this.cssID)
                 .attr('x1', PHRAGILE.Helpers.xOfDay)
                 .attr('y1', PHRAGILE.coordinateSystem.getY()(0))
                 .attr('x2', PHRAGILE.Helpers.xOfDay)
