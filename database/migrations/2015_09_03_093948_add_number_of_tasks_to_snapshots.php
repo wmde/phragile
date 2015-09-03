da9910a1cc4bc3b1155ed0ffa7758c301e8c61dd
Jakob Warkotsch <j.warkotsch@gmail.com>
2015-09-03 11:45:33 +0200

    Add task count to snapshots table.

---
+++ database/migrations/2015_09_03_093948_add_number_of_tasks_to_snapshots.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNumberOfTasksToSnapshots extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('sprint_snapshots', function($t)
+		{
+			$t->integer('task_count');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('sprint_snapshots', function($t)
+		{
+			$t->dropColumn('task_count');
+		});
+	}
+
+}
