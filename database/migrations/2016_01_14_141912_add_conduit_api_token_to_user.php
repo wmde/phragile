d8de3ff5e3aa4f3a8916fb31ce2876b1231003d9
WMDE-Fisch <christoph.fischer@wikimedia.de>
2016-01-14 14:30:14 +0100

    added field for Conduit API Token

---
+++ database/migrations/2016_01_14_141912_add_conduit_api_token_to_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddConduitAPITokenToUser extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->text('conduit_api_token');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropColumn('conduit_api_token');
+		});
+	}
+
+}
