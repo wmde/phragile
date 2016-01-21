f9583a66d862b4377f48e038b553be01c92b189c
WMDE-Fisch <christoph.fischer@wikimedia.de>
2016-01-21 12:37:15 +0100

    changed menu  settings to check for existing project

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -25,7 +25,7 @@
 								]
 							) !!}
 						</li>
-						@if(in_array(Route::currentRouteName(), ['project_path', 'sprint_path', 'sprint_live_path', 'snapshot_path']))
+						@if(in_array(Route::currentRouteName(), ['project_path', 'sprint_path', 'sprint_live_path', 'snapshot_path']) && isset($project))
 							<li>{!! link_to_route('create_sprint_path', 'New sprint', isset($project) ? $project->slug : $sprint->project->slug) !!}</li>
                             <li><a id="project-settings" href="#" data-toggle="modal" data-target="#project-settings-modal">Project settings</a></li>
                             @if(isset($sprint))
