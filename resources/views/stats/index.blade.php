5a9c9d5c3cd377ed2185d12cacb3a089c36ab649
Jakob Warkotsch <j.warkotsch@gmail.com>
2016-01-27 18:22:37 +0100

    Use the admin middleware instead of an inline check for stats.

--- resources/views/stats/index.blade.php
+++ resources/views/stats/index.blade.php
@@ -9,55 +9,49 @@
     <div class="container">
         <h1>Project statistics</h1>
 
-        @if(Auth::check() && Auth::user()->isInAdminList(env('PHRAGILE_ADMINS')))
-            <p>Overall numbers</p>
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th width="50%">Name</th>
-                    <th>Value</th>
-                </tr>
-                </thead>
-                <tbody class="list">
-                <tr>
-                    <td>Projects</td>
-                    <td>{!! $projects->count() !!}</td>
-                </tr>
-                <tr>
-                    <td>Sprints</td>
-                    <td>{!! $sprintCount !!}</td>
-                </tr>
-                </tbody>
-            </table>
+        <p>Overall numbers</p>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th width="50%">Name</th>
+                <th>Value</th>
+            </tr>
+            </thead>
+            <tbody class="list">
+            <tr>
+                <td>Projects</td>
+                <td>{!! $projects->count() !!}</td>
+            </tr>
+            <tr>
+                <td>Sprints</td>
+                <td>{!! $sprintCount !!}</td>
+            </tr>
+            </tbody>
+        </table>
 
-            <p>Sprints per project</p>
-            <table class="table table-striped">
-                <thead>
+        <p>Sprints per project</p>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th width="50%">Title</th>
+                <th>Count</th>
+            </tr>
+            </thead>
+            <tbody class="list">
+            @foreach($projects->get() as $project)
                 <tr>
-                    <th width="50%">Title</th>
-                    <th>Count</th>
+                    <td>
+                        {!! link_to_route(
+                            'project_path',
+                            $project->title,
+                            ['project' => $project->slug]
+                        ) !!}
+                    </td>
+                    <td>{{ $project->sprints->count() }}</td>
                 </tr>
-                </thead>
-                <tbody class="list">
-                @foreach($projects->get() as $project)
-                    <tr>
-                        <td>
-                            {!! link_to_route(
-                                'project_path',
-                                $project->title,
-                                ['project' => $project->slug]
-                            ) !!}
-                        </td>
-                        <td>{{ $project->sprints->count() }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        @else
-            <p>
-                Log in using your Phabricator account to see usage statistics.
-            </p>
-        @endif
+            @endforeach
+            </tbody>
+        </table>
     </div>
     <div class="clearfix"></div>
 </div>
