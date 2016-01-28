9855967a1c56340e5d6fbdb8747c3d0cd06e8c74
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-01-28 12:56:32 +0100

    Adjust the alignment of the title field help text. Remove colons from field labels.

--- resources/views/sprint/create.blade.php
+++ resources/views/sprint/create.blade.php
@@ -6,22 +6,23 @@
 	<h1>Add sprint for <span id="project-title">{{ $project->title }}</span></h1>
 	{!! Form::open(['method' => 'POST', 'route' => ['store_sprint_path', $project->slug]]) !!}
 		<div class="form-group">
-			{!! Form::label('sprint_start', 'Sprint start:') !!}
+			{!! Form::label('sprint_start', 'Sprint start') !!}
 			{!! Form::text('sprint_start', '', ['class' => 'form-control datepicker start']) !!}
 		</div>
 
 		<div class="form-group">
-			{!! Form::label('sprint_end', 'Sprint end:') !!}
+			{!! Form::label('sprint_end', 'Sprint end') !!}
 			{!! Form::text('sprint_end', '', ['class' => 'form-control datepicker end']) !!}
 		</div>
 
 		<div class="form-group">
-			{!! Form::label('title', 'Title*:') !!}
+			{!! Form::label('title', 'Title') !!}
 			{!! Form::text('title', '', ['class' => 'form-control', 'id' => 'sprint-title']) !!}
+			<p class="field-help">
+				Connect to an existing Phabricator project by entering its title or ID as the title.<br/>
+				If no project with the provided title exists on Phabricator yet, a new project with that title will be created.
+			</p>
 		</div>
-		<p>*Connect to an existing Phabricator project by entering its title or ID as the title.<br/>
-			&nbsp;If no project with the provided title exists on Phabricator yet, a new project with that title will be created.</p>
-
 
 		{!! Form::submit('Add sprint', ['class' => 'btn btn-primary']) !!}
 	{!! Form::close() !!}
