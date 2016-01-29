11ec55f72937807a59ecdb1103692d2a77d2b517
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-01-29 13:48:19 +0100

    Rearange before_script.sh slightly.
     - Start Phragile once Phabricator setup is done,
     - Provide more description to the nginx starting step (why ignore the error),
     - Rename nginx config file used by Phabricator.
    
    Those changes are mostly sort of preparation to having a HHVM
    CI build running.

--- build/before_script.sh
+++ build/before_script.sh
@@ -8,7 +8,6 @@ mysql -e 'create database phragile_test;'
 cp build/phragile.env .env
 cp build/travis_behat_custom.yml behat_custom.yml
 php artisan migrate --force
-php -S localhost:3030 public/index.php &
 
 # Phabricator Setup
 gunzip -c build/phabricator.sql.gz | mysql -u travis
@@ -26,6 +25,9 @@ phabricator/bin/storage upgrade -f
 
 cd -
 
+# Start Phragile
+php -S localhost:3030 public/index.php &
+
 # Start NGINX & PHP-FPM
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 if [ -e  ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ];
@@ -34,4 +36,4 @@ then
 fi
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-nginx -c `pwd`/build/phabricator.conf || true # nginx complains about something but should work anyway
+nginx -c `pwd`/build/nginx.conf || true # nginx emits an error when run as non-root user (but still starts). Do not break the build due to this.
