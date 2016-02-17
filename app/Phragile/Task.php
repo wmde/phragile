c38be8b81ac5939b626e5e9d8ca0d9a766032f47
Jakob Warkotsch <j.warkotsch@gmail.com>
2016-02-17 15:55:48 +0100

    Add Task type.

---
+++ app/Phragile/Task.php
@@ -0,0 +1,116 @@
+<?php
+namespace Phragile;
+
+class Task {
+	private $title;
+	private $priority;
+	private $points;
+	private $status;
+	private $closed;
+	private $id;
+	private $assigneePHID;
+	private $assigneeName;
+	private $cssClass;
+
+	function __construct($attributes)
+	{
+		$fields = ['title', 'priority', 'points', 'status', 'closed', 'id', 'assigneePHID'];
+		foreach ($fields as $field)
+		{
+			if (!key_exists($field, $attributes))
+			{
+				throw new \InvalidArgumentException("The $field field is missing.");
+			}
+
+			$this->$field = $attributes[$field];
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPriority()
+	{
+		return $this->priority;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPoints()
+	{
+		return $this->points;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStatus()
+	{
+		return $this->status;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isClosed()
+	{
+		return $this->closed;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAssigneePHID()
+	{
+		return $this->assigneePHID;
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function setAssigneeName($name)
+	{
+		$this->assigneeName = $name;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAssigneeName()
+	{
+		return $this->assigneeName;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCssClass()
+	{
+		return $this->cssClass;
+	}
+
+	/**
+	 * @param string $cssClass
+	 */
+	public function setCssClass($cssClass)
+	{
+		$this->cssClass = $cssClass;
+	}
+}
