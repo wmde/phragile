8c12525219bbe5afec0929264e6b3eb51cfff41d
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-05-11 14:50:13 +0200

    Convert task data in Phabricator's maniphest.search response format in snapshots

--- app/Console/Commands/lib/SnapshotTaskDataConverter.php
+++ app/Console/Commands/lib/SnapshotTaskDataConverter.php
@@ -2,16 +2,40 @@
 
 namespace App\Console\Commands\Lib;
 
+use Phragile\Domain\Task;
+use Phragile\TaskRawDataProcessor;
+
 class SnapshotTaskDataConverter {
 
 	public function convert(array $taskData)
 	{
-		return array_values(array_map(array($this, 'convertToManiphestSearchResponse'), $taskData));
+		if ($this->taskDataIsInManiphestQueryFormat($taskData))
+		{
+			return array_values(array_map([$this, 'convertTaskInManiphestQueryFormat'], $taskData));
+		}
+		if (!$this->taskDataIsInManiphestSearchFormat($taskData))
+		{
+			return $taskData;
+		}
+		$processor = new TaskRawDataProcessor();
+		return array_map(
+			function(Task $task)
+			{
+				return $task->getData();
+			},
+			$processor->process($taskData)
+		);
 	}
 
 	public function needsConversion(array $taskData)
 	{
-		return $this->taskDataIsInManiphestQueryFormat($taskData);
+		return $this->taskDataIsInManiphestSearchFormat($taskData) || $this->taskDataIsInManiphestQueryFormat($taskData);
+	}
+
+	private function taskDataIsInManiphestSearchFormat(array $taskData)
+	{
+		return array_keys($taskData) === range(0, count($taskData) - 1) && is_array($taskData[0]) &&
+		array_key_exists('fields', $taskData[0]) && array_key_exists('attachments', $taskData[0]);
 	}
 
 	private function taskDataIsInManiphestQueryFormat(array $taskData)
@@ -20,43 +44,31 @@ class SnapshotTaskDataConverter {
 		return array_keys($taskData) !== range(0, count($taskData) - 1);
 	}
 
-	private function convertToManiphestSearchResponse(array $task)
+	private function convertTaskInManiphestQueryFormat(array $task)
 	{
 		$points = isset(
 			$task['auxiliary'][env('MANIPHEST_STORY_POINTS_FIELD')]
 		) ? $task['auxiliary'][env('MANIPHEST_STORY_POINTS_FIELD')] : 0;
+
 		return [
 			'id' => (int)$task['id'],
-			'type' => 'TASK',
-			'phid' => $task['phid'],
-			'fields' => array_merge(
-				$this->convertCustomFields($task), [
-				'name' => $task['title'],
-				'authorPHID' => $task['authorPHID'],
-				'ownerPHID' => $task['ownerPHID'],
-				'status' => [
-					'value' => $task['status'],
-					'name' => null,
-					'color' => null
-				],
-				'priority' => [
-					'value' => null,
-					'subpriority' => null,
-					'name' => $task['priority'],
-				],
-				'points' => $points,
-			]),
-			'attachments' => ['projects' => ['projectPHIDs' => $task['projectPHIDs']]]
+			'title' => $task['title'],
+			'priority' => $task['priority'],
+			'status' => $task['status'],
+			'points' => $points,
+			'projectPHIDs' => $task['projectPHIDs'],
+			'assigneePHID' => $task['ownerPHID'],
+			'customFields' => $this->convertManiphestQueryCustomFields($task)
 		];
 	}
 
-	private function convertCustomFields(array $task)
+	private function convertManiphestQueryCustomFields(array $task)
 	{
 		$fields = [];
 
 		foreach ($task['auxiliary'] as $name => $value)
 		{
-			$fields['custom.' . $this->strAfterSecondColon($name)] = $value;
+			$fields[$this->extractFieldName($name)] = $value;
 		}
 
 		return $fields;
@@ -68,8 +80,12 @@ class SnapshotTaskDataConverter {
 	 * @param string $s
 	 * @return string
 	 */
-	private function strAfterSecondColon($s)
+	private function extractFieldName($s)
 	{
+		if (count(explode(':', $s)) < 3)
+		{
+			return $s;
+		}
 		return implode(':',
 			array_slice(explode(':', $s), 2)
 		);
--- tests/unit/SnapshotTaskDataConverterTest.php
+++ tests/unit/SnapshotTaskDataConverterTest.php
@@ -5,70 +5,105 @@ namespace Phragile\Tests;
 use App\Console\Commands\Lib\SnapshotTaskDataConverter;
 
 class SnapshotTaskDataConverterTest extends TestCase {
-	public function testNeedsConversionRecognizesManiphestQueryData()
+
+	public function testGivenManiphestQueryData_needsConversionReturnsTrue()
 	{
 		$converter = new SnapshotTaskDataConverter();
-		$this->assertTrue($converter->needsConversion($this->queryTaskData));
+		$this->assertTrue($converter->needsConversion($this->getQueryTaskData()));
 	}
 
-	public function testGivenManiphestQueryData_ReturnsManiphestSearchData()
+	public function testGivenManiphestSearchData_needsConversionReturnsTrue()
 	{
-		$customField = 'WMDE:storypoints';
-		$this->queryTaskData['PHID-TASK-4kvxc4re6xrshgxtfajl']['auxiliary']['std:maniphest:' . $customField] = 'foo';
-		$this->queryTaskData['PHID-TASK-4kvxc4re6xrshgxtfajl']['auxiliary'][env('MANIPHEST_STORY_POINTS_FIELD')] = '5';
-		$convertedData = (new SnapshotTaskDataConverter())->convert($this->queryTaskData);
-
-		$this->assertSame($this->searchTaskData[0]['id'], $convertedData[0]['id']);
-		$this->assertSame($this->searchTaskData[0]['phid'], $convertedData[0]['phid']);
-		$this->assertSame($this->searchTaskData[0]['fields']['name'], $convertedData[0]['fields']['name']);
-		$this->assertSame($this->searchTaskData[0]['fields']['authorPHID'], $convertedData[0]['fields']['authorPHID']);
-		$this->assertSame($this->searchTaskData[0]['fields']['ownerPHID'], $convertedData[0]['fields']['ownerPHID']);
-		$this->assertSame($this->searchTaskData[0]['fields']['status']['value'], $convertedData[0]['fields']['status']['value']);
-		$this->assertSame($this->searchTaskData[0]['fields']['priority']['name'], $convertedData[0]['fields']['priority']['name']);
-		$this->assertSame($this->searchTaskData[0]['fields']['points'], $convertedData[0]['fields']['points']);
-		$this->assertSame('foo', $convertedData[0]['fields']['custom.' . $customField]);
-		$this->assertSame(
-			$this->queryTaskData['PHID-TASK-4kvxc4re6xrshgxtfajl']['projectPHIDs'],
-			$convertedData[0]['attachments']['projects']['projectPHIDs']
-		);
+		$converter = new SnapshotTaskDataConverter();
+		$this->assertTrue($converter->needsConversion($this->searchTaskData));
 	}
 
-	private $queryTaskData = [
-		'PHID-TASK-4kvxc4re6xrshgxtfajl' => [
-			'id' => '127180',
-			'phid' => 'PHID-TASK-4kvxc4re6xrshgxtfajl',
-			'authorPHID' => 'PHID-USER-t4sxxglz6yyrgxeib43i',
-			'ownerPHID' => 'PHID-USER-5dv7dcltvyvolwzbm2af',
-			'ccPHIDs' => [
-				'PHID-USER-5dv7dcltvyvolwzbm2af',
-				'PHID-USER-fn7qnpccfbitivgtw2rt',
-				'PHID-USER-lltif2drabccdkwhet7x'
-			],
-			'status' => 'open',
-			'statusName' => 'Open',
-			'isClosed' => false,
-			'priority' => 'High',
-			'priorityColor' => 'red',
-			'title' => '[Phragile] Migration script for old snapshots',
-			'description' => 'Snapshot data needs to be migrated to a new format since we are going to
+	public function testGivenConvertedData_needsConversionReturnsFalse()
+	{
+		$converter = new SnapshotTaskDataConverter();
+		$this->assertFalse($converter->needsConversion($this->getConvertedData()));
+	}
+
+	public function testGivenDataInUnsupportedFormat_needsConversionReturnsFalse()
+	{
+		$converter = new SnapshotTaskDataConverter();
+		$this->assertFalse($converter->needsConversion([['foo' => 'bar']]));
+	}
+
+	public function testGivenManiphestQueryData_convertReturnsConvertedData()
+	{
+		$expectedConvertedData = $this->getConvertedData();
+		// TODO: this is a bit silly but as long as tests use the production .env file there is
+		// no value of MANIPHEST_STORY_POINTS_FIELD test could rely on. This could be possibly
+		// solved by introducing test .env file with some particular value MANIPHEST_STORY_POINTS_FIELD
+		$customStoryPointsField = $this->strAfterSecondColon(env('MANIPHEST_STORY_POINTS_FIELD'));
+		$expectedConvertedData[0]['customFields']['WMDE:storypoints'] = 'foo';
+		$expectedConvertedData[0]['customFields'][$customStoryPointsField] = '5';
+
+		$queryTaskData = $this->getQueryTaskData();
+		$customField = 'WMDE:storypoints';
+		$queryTaskData['PHID-TASK-4kvxc4re6xrshgxtfajl']['auxiliary']['std:maniphest:' . $customField] = 'foo';
+		$queryTaskData['PHID-TASK-4kvxc4re6xrshgxtfajl']['auxiliary'][env('MANIPHEST_STORY_POINTS_FIELD')] = '5';
+		$convertedTaskData = (new SnapshotTaskDataConverter())->convert($queryTaskData);
+
+		$this->assertSame($expectedConvertedData, $convertedTaskData);
+	}
+
+	public function testGivenManiphestSearchData_convertReturnsConvertedData()
+	{
+		$expectedConvertedData = $this->getConvertedData();
+		$expectedConvertedData[0]['customFields']['external_reference'] = null;
+		$convertedTaskData = (new SnapshotTaskDataConverter())->convert($this->searchTaskData);
+
+		$this->assertSame($expectedConvertedData, $convertedTaskData);
+	}
+
+	public function testGivenConvertedData_convertReturnsUnchangedData()
+	{
+		$expectedConvertedData = $this->getConvertedData();
+		$convertedTaskData = (new SnapshotTaskDataConverter())->convert($this->getConvertedData());
+		$this->assertSame($expectedConvertedData, $convertedTaskData);
+	}
+
+	private function getQueryTaskData()
+	{
+		return [
+			'PHID-TASK-4kvxc4re6xrshgxtfajl' => [
+				'id' => '127180',
+				'phid' => 'PHID-TASK-4kvxc4re6xrshgxtfajl',
+				'authorPHID' => 'PHID-USER-t4sxxglz6yyrgxeib43i',
+				'ownerPHID' => 'PHID-USER-5dv7dcltvyvolwzbm2af',
+				'ccPHIDs' => [
+					'PHID-USER-5dv7dcltvyvolwzbm2af',
+					'PHID-USER-fn7qnpccfbitivgtw2rt',
+					'PHID-USER-lltif2drabccdkwhet7x'
+				],
+				'status' => 'open',
+				'statusName' => 'Open',
+				'isClosed' => false,
+				'priority' => 'High',
+				'priorityColor' => 'red',
+				'title' => '[Phragile] Migration script for old snapshots',
+				'description' => 'Snapshot data needs to be migrated to a new format since we are going to
 				abandon maniphest.query in favor of maniphest.search.',
-			'projectPHIDs' => [
-				'PHID-PROJ-ptnfbfyq36kkebaxugcz',
-				'PHID-PROJ-tazsyaydzpbd643tderv',
-				'PHID-PROJ-knyj2bgnrkrwu72n27bg'
-			],
-			'uri' => 'https://phabricator.wikimedia.org/T127180',
-			'auxiliary' => [
-				'std:maniphest:security_topic' => 'default',
-			],
-			'objectName' => 'T127180',
-			'dateCreated' => '1455716487',
-			'dateModified' => '1455880296',
-			'dependsOnTaskPHIDs' => [
-				'PHID-TASK-tuaxg2zcafwmpoe2d5ys'
+				'projectPHIDs' => [
+					'PHID-PROJ-ptnfbfyq36kkebaxugcz',
+					'PHID-PROJ-tazsyaydzpbd643tderv',
+					'PHID-PROJ-knyj2bgnrkrwu72n27bg'
+				],
+				'uri' => 'https://phabricator.wikimedia.org/T127180',
+				'auxiliary' => [
+					'std:maniphest:security_topic' => 'default',
+				],
+				'objectName' => 'T127180',
+				'dateCreated' => '1455716487',
+				'dateModified' => '1455880296',
+				'dependsOnTaskPHIDs' => [
+					'PHID-TASK-tuaxg2zcafwmpoe2d5ys'
+				]
 			]
-		]
-	];
+		];
+	}
 
 	private $searchTaskData = [
 		[
@@ -112,4 +147,32 @@ class SnapshotTaskDataConverterTest extends TestCase {
 			]
 		]
 	];
+
+	private function getConvertedData()
+	{
+		return [
+			[
+				'id' => 127180,
+				'title' => '[Phragile] Migration script for old snapshots',
+				'priority' => 'High',
+				'status' => 'open',
+				'points' => '5',
+				'projectPHIDs' => [
+					'PHID-PROJ-ptnfbfyq36kkebaxugcz',
+					'PHID-PROJ-tazsyaydzpbd643tderv',
+					'PHID-PROJ-knyj2bgnrkrwu72n27bg'
+				],
+				'assigneePHID' => 'PHID-USER-5dv7dcltvyvolwzbm2af',
+				'customFields' => ['security_topic' => 'default'],
+			]
+		];
+	}
+
+	private function strAfterSecondColon($s)
+	{
+		return implode(':',
+			array_slice(explode(':', $s), 2)
+		);
+	}
+
 }
